c9acc0fc8756ba4279f962defab27dc93c7e5209
Alfred <alfredofernandezcarlos@gmail.com>
2024-11-04 09:59:49 -0400

    refactor: remove books and movies files

--- data/navigation.ts
+++ data/navigation.ts
@@ -2,21 +2,21 @@ import { SITE_METADATA } from './site-metadata'
 
 export const HEADER_NAV_LINKS = [
   { href: '/blog', title: 'Blog', emoji: 'writing-hand' },
-  { href: '/snippets', title: 'Snippets', emoji: 'dna' },
+  // { href: '/snippets', title: 'Snippets', emoji: 'dna' },
   { href: '/projects', title: 'Projects', emoji: 'man-technologist' },
   { href: '/about', title: 'About', emoji: 'billed-cap' },
 ]
 
 export const MORE_NAV_LINKS = [
-  { href: '/books', title: 'Books', emoji: 'books' },
-  { href: '/movies', title: 'Movies', emoji: 'film-frames' },
+  // { href: '/books', title: 'Books', emoji: 'books' },
+  // { href: '/movies', title: 'Movies', emoji: 'film-frames' },
   { href: '/tags', title: 'Tags', emoji: 'label' },
   // { href: SITE_METADATA.analyticsUrl, title: 'Blog stats', emoji: 'bar-chart' },
 ]
 
 export const FOOTER_NAV_LINKS = [
   { href: '/blog', title: 'Blog' },
-  { href: '/snippets', title: 'Snippets' },
+  // { href: '/snippets', title: 'Snippets' },
   { href: '/projects', title: 'Projects' },
   { href: '/tags', title: 'Tags' },
   { href: '/feed.xml', title: 'RSS feed' },
@@ -25,7 +25,7 @@ export const FOOTER_NAV_LINKS = [
 export const FOOTER_PERSONAL_STUFF = [
   { href: '/about', title: 'About' },
   { href: '/static/resume.pdf', title: 'Resume' },
-  { href: '/books', title: 'Books' },
-  { href: '/movies', title: 'Movies' },
+  // { href: '/books', title: 'Books' },
+  // { href: '/movies', title: 'Movies' },
   // { href: SITE_METADATA.analyticsUrl, title: 'Blog stats' },
 ]
